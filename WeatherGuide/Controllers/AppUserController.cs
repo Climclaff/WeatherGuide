7d4a2ee520642c2bec00359ccf345f5922ca6585
Climclaff <83221676+Climclaff@users.noreply.github.com>
2021-11-09 15:16:50 +0200

    Added user controller/ package updates

---
+++ WeatherGuide/Controllers/AppUserController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WeatherGuide.Data;
+using WeatherGuide.Models;
+
+namespace WeatherGuide.Controllers
+{
+    public class AppUserController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AppUserController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: AppUser
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Users.Include(a => a.Country).Include(a => a.State);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: AppUser/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var appUser = await _context.Users
+                .Include(a => a.Country)
+                .Include(a => a.State)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (appUser == null)
+            {
+                return NotFound();
+            }
+
+            return View(appUser);
+        }
+
+        // GET: AppUser/Create
+        public IActionResult Create()
+        {
+            ViewData["CountryId"] = new SelectList(_context.Countries, "Id", "Id");
+            ViewData["StateId"] = new SelectList(_context.States, "Id", "Id");
+            return View();
+        }
+
+        // POST: AppUser/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CountryId,StateId,Id,UserName,NormalizedUserName,Email,NormalizedEmail,EmailConfirmed,PasswordHash,SecurityStamp,ConcurrencyStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEnd,LockoutEnabled,AccessFailedCount")] AppUser appUser)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(appUser);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CountryId"] = new SelectList(_context.Countries, "Id", "Id", appUser.CountryId);
+            ViewData["StateId"] = new SelectList(_context.States, "Id", "Id", appUser.StateId);
+            return View(appUser);
+        }
+
+        // GET: AppUser/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var appUser = await _context.Users.FindAsync(id);
+            if (appUser == null)
+            {
+                return NotFound();
+            }
+            ViewData["CountryId"] = new SelectList(_context.Countries, "Id", "Id", appUser.CountryId);
+            ViewData["StateId"] = new SelectList(_context.States, "Id", "Id", appUser.StateId);
+            return View(appUser);
+        }
+
+        // POST: AppUser/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CountryId,StateId,Id,UserName,NormalizedUserName,Email,NormalizedEmail,EmailConfirmed,PasswordHash,SecurityStamp,ConcurrencyStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEnd,LockoutEnabled,AccessFailedCount")] AppUser appUser)
+        {
+            if (id != appUser.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(appUser);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AppUserExists(appUser.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CountryId"] = new SelectList(_context.Countries, "Id", "Id", appUser.CountryId);
+            ViewData["StateId"] = new SelectList(_context.States, "Id", "Id", appUser.StateId);
+            return View(appUser);
+        }
+
+        // GET: AppUser/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var appUser = await _context.Users
+                .Include(a => a.Country)
+                .Include(a => a.State)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (appUser == null)
+            {
+                return NotFound();
+            }
+
+            return View(appUser);
+        }
+
+        // POST: AppUser/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var appUser = await _context.Users.FindAsync(id);
+            _context.Users.Remove(appUser);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AppUserExists(int id)
+        {
+            return _context.Users.Any(e => e.Id == id);
+        }
+    }
+}
